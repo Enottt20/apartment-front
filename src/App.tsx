8c4f2261126b5859bb633eebead89a2562b9a2e0
Enottt20 <levcorp03@gmail.com>
2024-10-13 20:36:02 +0300

    about

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom';
+import About from './pages/About';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
